1adec4834cd28eb277c0e176d73e21f51e29a1bf
Kronik502 <kronikwenaite@gmail.com>
2024-10-21 10:50:04 +0200

    New updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import LocationInput from './components/LocationInput';
@@ -12,33 +12,67 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import Login from './login';
 import Signup from './signup';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 import './components/WeatherDisplay.css';
-import './components/custom.scss'; // Ensure this file exists and is correctly imported
+import './components/custom.scss';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const user = localStorage.getItem('currentUser');
+    if (user) {
+      setCurrentUser(JSON.parse(user));
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('currentUser');
+    setCurrentUser(null);
+  };
+
   return (
-    <Provider className='appp' store={store}>
+    <Provider store={store}>
       <BrowserRouter>
         <div>
-          <Navbar expand="lg" className="bg-primary"> {/* Adjusted to a class that reflects your theme */}
+          <Navbar expand="lg" className="bg-primary">
             <Container>
               <Navbar.Brand href="#home">Weather Kronikles</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                  <Nav.Link href="/signup">Sign Up</Nav.Link>
-                  <Nav.Link href="/login">Login</Nav.Link>
-                  <Nav.Link href="/weather">Weather App</Nav.Link>
+                  {currentUser ? (
+                    <>
+                      <Nav.Link href="/weather">Weather App</Nav.Link>
+                      <Nav.Link href="/" onClick={handleLogout}>Logout</Nav.Link>
+                    </>
+                  ) : (
+                    <>
+                      <Nav.Link href="/signup">Sign Up</Nav.Link>
+                      <Nav.Link href="/login">Login</Nav.Link>
+                    </>
+                  )}
                 </Nav>
               </Navbar.Collapse>
             </Container>
           </Navbar>
           <Routes>
             <Route path="/" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login onLogin={setCurrentUser} />} />
             <Route path="/signup" element={<Signup />} />
-            <Route path="/weather" element={<div><LocationInput /><WeatherDisplay /><Forecast /><WeatherAlerts /><OfflineMode /><Settings /></div>} />
+            <Route path="/weather" element={
+              <ProtectedRoute>
+                <div>
+                  <LocationInput />
+                  <WeatherDisplay />
+                  <Forecast />
+                  <WeatherAlerts />
+                  <OfflineMode />
+                  <Settings />
+                </div>
+              </ProtectedRoute>
+            } />
           </Routes>
         </div>
       </BrowserRouter>
